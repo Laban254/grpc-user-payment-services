5a5698632b2238c0f7d85cdf8de157b8cac00c73
laban254 <labanrotich6544@gmail.com>
2024-10-23 12:41:56 +0300

    Add Transaction model to database models

--- database/models.go
+++ database/models.go
@@ -6,3 +6,10 @@ type User struct {
     Name  string `gorm:"size:100"`
     Email string `gorm:"uniqueIndex;size:100"`
 }
+
+type Transaction struct {
+    ID        int32   `gorm:"primaryKey" json:"id"`      
+    UserID    int32   `gorm:"not null" json:"user_id"`      
+    Amount    float64 `gorm:"not null" json:"amount"`      
+    Status    string  `gorm:"not null" json:"status"`     
+}
